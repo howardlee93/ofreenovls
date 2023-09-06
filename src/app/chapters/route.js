0a001ff652dfeb8354d49c26eb8f8642538a2601
Howard <howardl93@yahoo.com>
2023-09-06 11:18:36 -0700

    chapt routes

--- src/app/chapters/route.js
+++ src/app/chapters/route.js
@@ -1,12 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import {NextResponse} from 'next/server';
+
+const prisma = new PrismaClient();
 
 export const GET = async(req, res)=>{
 
 }
 
 
-export const POST = async(req, res)=>{
+export const POST = async(req, res)=>{ // add new chapter
+    const { title, summary, workId, content} = await res.json()
     
+    const newChapt = await prisma.chapter.create({
+        title, 
+        summary,
+        content,
+        work: {
+            connect:{
+                where: {id: workId}
+            }
+        }
+    })
+    return NextResponse.json(newChapt);
 }
 
 
