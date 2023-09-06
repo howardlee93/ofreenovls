8bbe50438f68e296893a0ec43007ad9a711827eb
Howard <howardl93@yahoo.com>
2023-09-06 11:10:47 -0700

    chapters api

---
+++ src/app/chapters/route.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+export const GET = async(req, res)=>{
+
+}
+
+
+export const POST = async(req, res)=>{
+    
+}
+
+
+export const PUT = async(req, res)=>{
+    
+}
+
+
+export const DELETE = async(req, res)=>{
+    
+}
