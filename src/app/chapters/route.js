973beeb857dba232de5cc86b39bfcec3b89ec401
Howard <howardl93@yahoo.com>
2023-09-06 16:04:37 -0700

    chapter routes

--- src/app/chapters/route.js
+++ src/app/chapters/route.js
@@ -9,27 +9,50 @@ export const GET = async(req, res)=>{
 
 
 export const POST = async(req, res)=>{ // add new chapter
-    const { title, summary, workId, content} = await res.json()
+    const { title, summary, workId, content} = await req.json()
     
     const newChapt = await prisma.chapter.create({
-        title, 
-        summary,
-        content,
-        work: {
-            connect:{
-                where: {id: workId}
+        data:{
+            title, 
+            summary,
+            content,
+            work: {
+                connect:{
+                    id: parseInt(workId)
+                }
             }
-        }
+        },
     })
     return NextResponse.json(newChapt);
 }
 
 
 export const PUT = async(req, res)=>{
-    
+    const { title, summary, workId, content, id} = await req.json()
+    console.log(typeof id);
+    const updatedChapt = await prisma.chapter.update({
+        where:{
+            id,
+            // workId: parseInt(workId)
+        },
+        data:{  
+            title, 
+            summary,
+            content,
+        }
+    })
+    return NextResponse.json(updatedChapt);
 }
 
 
 export const DELETE = async(req, res)=>{
+    const {id} = await req.json();
     
+    const deletedChapt = await prisma.chapter.delete({
+        where:{
+            id
+        },
+    })
+
+    return NextResponse.json(deletedChapt);
 }
