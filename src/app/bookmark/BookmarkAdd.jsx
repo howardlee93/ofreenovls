5347444871446b0d868ba03f8ffcbc8616b60391
Howard <howardl93@yahoo.com>
2023-09-09 09:43:23 -0700

    bookmark route

---
+++ src/app/bookmark/BookmarkAdd.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+const BookmarkAdd = props=>{
+
+}
+
+export default BookmarkAdd;
---
+++ src/app/bookmark/BookmarkDisplay.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+const BookmarkDisplay = props=>{
+
+}
+
+export default BookmarkDisplay;
