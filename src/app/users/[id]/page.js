7f8d70fb05b2d417f697a7a1f6ecd12e0776a189
Howard <howardl93@yahoo.com>
2023-09-15 15:54:23 -0700

    user layouts

--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
 import BookmarkDisplay from "@/app/bookmark/BookmarkDisplay";
-
+import styles from './layout.module.css'
 
 const UserDashboard = async ({params})=>{
     const prisma = new PrismaClient();
@@ -27,13 +27,13 @@ const UserDashboard = async ({params})=>{
     return(
         <>
             <h1>{user.name}!</h1>
-            <h2>Works</h2>
+            <h2 className={styles.userHeader}>Works</h2>
             {user.works.slice(0,5).map(work=>{
                 return(
                     <p key={work.id}><Link href={`/works/${work.id}`}>{work.title}</Link></p>
                 )
             })}
-            <h2>Bookmarks</h2>
+            <h2 className={styles.userHeader}>Bookmarks</h2>
             {user.bookmarks.slice(0,5).map(b=>{
                 return(
                     <BookmarkDisplay key={b.id} bookmark={b}/>
